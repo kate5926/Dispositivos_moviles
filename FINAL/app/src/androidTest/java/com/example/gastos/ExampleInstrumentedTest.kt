0c7b8293b012850cfe2182f54bf44f8433ff2521
esth-12343 <echungap@ulasalle.edu.pe>
2025-12-06 22:29:41 -0500

    Add files via upload

--- FINAL/app/src/androidTest/java/com/example/gastos/ExampleInstrumentedTest.kt
+++ FINAL/app/src/androidTest/java/com/example/gastos/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.gastos
+package es.uva.sg.psm.planificadorfinanciero
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.gastos", appContext.packageName)
+        assertEquals("es.uva.sg.psm.planificadorfinanciero", appContext.packageName)
     }
 }
